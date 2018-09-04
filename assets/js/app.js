8c4e9d6a526af7784e1a3cc5966ef787584f3bf5
Aleksandar Atlagic <atlagic.develop@gmail.com>
2018-09-05 00:01:47 +0200

    removing unnecessary js files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import Home from './pages/Home.jsx';
+import Home from './pages/Home.jsx';
 //import SingleProduct from "./pages/SingleProduct";
-import Categories from "./pages/Categories";
+//import Categories from "./pages/Categories";
 
 class App extends React.Component {
-
     render() {
         return (
-            <Categories />
+            <Home />
         );
     }
 }
--- assets/js/components/Search.jsx
+++ assets/js/components/Search.jsx
@@ -1,7 +1,17 @@
 import React, { Component }  from 'react';
-import MainNav from "./MainNav";
 
 class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: false
+        };
+    }
+
+    toggleCategory() {
+        this.setState({active: !this.state.active});
+    }
+
     render() {
         return (
             <div className="header_search_content">
@@ -10,9 +20,9 @@ class Search extends Component {
                         <input type="search" required="required" className="header_search_input" placeholder="Search for products..."/>
                         <div className="custom_dropdown">
                             <div className="custom_dropdown_list">
-                                <span className="custom_dropdown_placeholder clc">All Categories</span>
+                                <span className="custom_dropdown_placeholder clc" onClick={this.toggleCategory.bind(this)}>All Categories</span>
                                 <i className="fas fa-chevron-down"></i>
-                                <ul className="custom_list clc">
+                                <ul className={`custom_list clc ${this.state.active ? 'active' : ''}`}>
                                     <li><a className="clc" href="#">All Categories</a></li>
                                     <li><a className="clc" href="#">Computers</a></li>
                                     <li><a className="clc" href="#">Laptops</a></li>
