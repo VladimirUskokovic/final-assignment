56d24d6a72873b9e32c614c1ae55ecc902db3057
Vladimir Uskokovic <vladimir.uskokovic@byteout.com>
2019-03-16 18:39:07 +0100

    set global variables

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,23 +4,21 @@ import Home from './pages/Home.jsx';
 import SingleProduct from "./pages/SingleProduct";
 import Categories from "./pages/Categories";
 import {BrowserRouter, NavLink, Route, Switch} from 'react-router-dom';
+import global from './util/global';
 
 class App extends React.Component {
-    render() {
-      const DEV_PATH = '/final-assignment/public/client';
-      const PATH = '/client';
-      const DEV = true;
-
-        return (
-          <BrowserRouter>
-              <Switch>
-                <Route exact path={DEV ? DEV_PATH : PATH} component={Home}/>
-                <Route path={`${DEV ? DEV_PATH : PATH}/product/:product_id`} component={SingleProduct}/>
-                <Route path={`${DEV ? DEV_PATH : PATH}/category/:category_id`} component={Categories}/>
-              </Switch>
-          </BrowserRouter>
-        );
-    }
+  render() {
+    console.log(global);
+      return (
+        <BrowserRouter>
+          <Switch>
+            <Route exact path={`${global.REACT_PATH}`} component={Home}/>
+            <Route path={`${global.REACT_PATH}/product/:product_id`} component={SingleProduct}/>
+            <Route path={`${global.REACT_PATH}/category/:category_id`} component={Categories}/>
+          </Switch>
+        </BrowserRouter>
+      );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- assets/js/components/ProductLarge.jsx
+++ assets/js/components/ProductLarge.jsx
@@ -1,59 +1,62 @@
 import React, { Component } from "react";
+import global from '../util/global';
 
 class ProductLarge extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            image: "images/single_1.jpg",
-            images: [
-                "images/single_1.jpg",
-                "images/single_2.jpg",
-                "images/single_3.jpg"
-            ]
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: `${global.PATH}/images/single_1.jpg`,
+      images: [
+        `${global.PATH}/images/single_1.jpg`,
+        `${global.PATH}/images/single_2.jpg`,
+        `${global.PATH}/images/single_3.jpg`
+      ]
     }
-    setModal(imageKey) {
-        this.setState({ image: this.state.images[imageKey] });
-    }
-    getImage() {
-        return this.state.image;
-    }
-    render() {
-        let images = this.state.images.map((val, key) => {
-            return <li onClick={() => {this.setModal(key)}} key={key}><img src={val} alt=""/></li>
-        });
-        return (
-            <div>
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-2 order-lg-1 order-2">
-                        <ul className="image_list">
-                            {images}
-                        </ul>
-                    </div>
-                    <div className="col-lg-5 order-lg-2 order-1">
-                        <div className="image_selected">
-                            <img src={this.state.image}></img>
-                        </div>
-                    </div>
-                    <div className="col-lg-5 order-3">
-                        <div className="product_description">
-                            <div className="product_category">Laptops</div>
-                            <div className="product_name">MacBook Air 13</div>
-                            <div className="rating_r rating_r_4 product_rating"><i></i><i></i><i></i><i></i><i></i>
-                            </div>
-                            <div className="product_text"><p>Lorem ipsum dolor sit amet, consectetur adipiscing
-                                elit. Maecenas fermentum. laoreet turpis, nec sollicitudin dolor cursus at. Maecenas
-                                aliquet, dolor a faucibus efficitur, nisi tellus cursus urna, eget dictum lacus
-                                turpis.</p></div>
+  }
+  setModal(imageKey) {
+    this.setState({ image: this.state.images[imageKey] });
+  }
+  getImage() {
+    return this.state.image;
+  }
+  render() {
+    let images = this.state.images.map((val, key) => <li onClick={() => {this.setModal(key)}} key={key}><img src={val} alt=""/></li>);
 
-                            <div className="product_price">$2000 - $2150</div>
-                        </div>
-                    </div>
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-2 order-lg-1 order-2">
+              <ul className="image_list">
+                {images}
+              </ul>
+            </div>
+            <div className="col-lg-5 order-lg-2 order-1">
+              <div className="image_selected">
+                <img src={this.state.image}></img>
                 </div>
-            </div></div>
-        );
-    }
+            </div>
+            <div className="col-lg-5 order-3">
+               <div className="product_description">
+                  <div className="product_category">Laptops</div>
+                  <div className="product_name">MacBook Air 13</div>
+                  <div className="product_text">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing
+                      elit. Maecenas fermentum. laoreet turpis, nec sollicitudin dolor cursus at. Maecenas
+                      aliquet, dolor a faucibus efficitur, nisi tellus cursus urna, eget dictum lacus
+                      turpis.
+                    </p>
+                  </div>
+
+                  <div className="product_price">$2000 - $2150</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ProductLarge
