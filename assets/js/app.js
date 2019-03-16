2f9ac4535f6e73e1d0d43f445f4b4dd33b60c059
Aleksandar Atlagic <atlagic.develop@gmail.com>
2019-03-16 17:36:50 +0100

    react router

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import Home from './pages/Home.jsx';
+import Home from './pages/Home.jsx';
 import SingleProduct from "./pages/SingleProduct";
-//import Categories from "./pages/Categories";
+import Categories from "./pages/Categories";
+import {BrowserRouter, NavLink, Route, Switch} from 'react-router-dom';
 
 class App extends React.Component {
     render() {
+      const def = 'client';
+
         return (
-            <SingleProduct/>
+          <BrowserRouter>
+              <Switch>
+                <Route exact path="client" component={Home}/>
+                <Route path="client/product/:product_id" component={SingleProduct}/>
+                <Route path="client/category" component={Categories}/>
+                // <Route path="/category/:category_id" component={Categories}/>
+              </Switch>
+          </BrowserRouter>
         );
     }
 }
