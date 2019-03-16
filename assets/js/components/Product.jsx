76a7f677c4e3afe0a0f4e853aa5593de82dcddef
Vladimir Uskokovic <vladimir.uskokovic@byteout.com>
2019-03-16 18:48:11 +0100

    image path fixes

--- assets/js/components/Product.jsx
+++ assets/js/components/Product.jsx
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
+import global from '../util/global';
 
 class Product extends Component {
-    render() {
-        return (
-            <div className="product_item discount d-flex flex-column align-items-center justify-content-center text-center">
-                <div className="product_image d-flex flex-column align-items-center justify-content-center">
-                    <img src="images/featured_1.png" alt=""/></div>
-                <div className="product_content">
-                    <div className="product_price discount">$225 - $300</div>
-                    <div className="product_name">
-                        <div><a href="product.html">Huawei MediaPad...</a></div>
-                    </div>
-                    <div className="product_extras">
-                        <button className="product_cart_button">Check Prices</button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="product_item discount d-flex flex-column align-items-center justify-content-center text-center">
+        <div className="product_image d-flex flex-column align-items-center justify-content-center">
+          <img src={`${global.PATH}/images/featured_1.png`} alt=""/></div>
+        <div className="product_content">
+          <div className="product_price discount">$225 - $300</div>
+          <div className="product_name">
+            <div><a href="product.html">Huawei MediaPad...</a></div>
+          </div>
+          <div className="product_extras">
+            <button className="product_cart_button">Check Prices</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Product
